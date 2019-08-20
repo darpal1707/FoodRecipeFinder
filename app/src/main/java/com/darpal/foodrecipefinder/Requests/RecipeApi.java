9b9d94535640e14eb13159dda4368c2916715515
darpal1995 <darpaldhyani2014@gmail.com>
2019-08-20 14:12:12 -0700

    @Get requests

--- app/src/main/java/com/darpal/foodrecipefinder/Requests/RecipeApi.java
+++ app/src/main/java/com/darpal/foodrecipefinder/Requests/RecipeApi.java
@@ -1,4 +1,25 @@
 package com.darpal.foodrecipefinder.Requests;
 
+import com.darpal.foodrecipefinder.Requests.Responses.RecipeResponse;
+import com.darpal.foodrecipefinder.Requests.Responses.RecipeSearchResponse;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
 public interface RecipeApi {
+
+    //Search Recipe
+    @GET("api/search")
+    Call<RecipeSearchResponse> searchRecipe(
+            @Query("key") String key,
+            @Query("q") String query,
+            @Query("page") String page
+    );
+
+    @GET("api/get")
+    Call<RecipeResponse> recipeResponse(
+            @Query("key") String key,
+            @Query("rid") String recipe_id
+    );
 }
---
+++ app/src/main/java/com/darpal/foodrecipefinder/Requests/Responses/RecipeResponse.java
@@ -0,0 +1,22 @@
+package com.darpal.foodrecipefinder.Requests.Responses;
+
+import com.darpal.foodrecipefinder.Model.Recipe;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class RecipeResponse {
+    @SerializedName("recipe")
+    @Expose()
+    private Recipe recipe;
+
+    public Recipe getRecipe(){
+        return recipe;
+    }
+
+    @Override
+    public String toString() {
+        return "RecipeResponse{" +
+                "recipe=" + recipe +
+                '}';
+    }
+}
